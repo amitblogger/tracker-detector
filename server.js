2878d9d15b907eea3608737f28564119daf6023d
amitblogger <amitblogger999@gmail.com>
2025-03-15 00:24:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
